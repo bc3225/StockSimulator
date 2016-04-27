de34b88c12d399d56bb1bb94f2292576345038bf
Graham Simpson <simpson.graham@outlook.com>
2016-04-27 12:39:58 -0400

    Added functionality to read in the csv

---
+++ WorkspaceProject/StockSimulatorBGSZ0.3/src/CalcPortfolio.java
@@ -0,0 +1,17 @@
+//Graham Simpson
+//4/25/16
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CalcPortfolio{
+	
+	// read in the csv file
+	Portfolio p = new Portfolio();
+	p.readFile("C:\\Code Workspace\\StockSimulator\\WorkspaceProject\\StockSimulatorBGSZ0.3\\bgszExchangePortfolio.csv");
+	
+	
+}
+

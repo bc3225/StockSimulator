7f1685de0d1ea0bc0e7e6c26bf311f534a98d82e
Brian Cruts <bc3225@desales.edu>
2016-04-29 15:24:12 -0400

    passed file path from save to the calcPortfolio method, fixed UI bug
    Changes started by Brian finished by Graham and Zach!

--- WorkspaceProject/StockSimulatorBGSZ0.3/src/CalcPortfolio.java
+++ WorkspaceProject/StockSimulatorBGSZ0.3/src/CalcPortfolio.java
@@ -7,11 +7,31 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class CalcPortfolio{
+	 private String[] holdings;
+	 
+	 //need to calc total port value
+	 //be able to subtract shares of a single stock (sell)
+	 // add shares of a single stock (buy)
+	 //when the program opens update all the holdings values
+	 //error checking
+	 //write all the info to the table in the UI
+	 
 	
-	// read in the csv file
-	Portfolio p = new Portfolio();
-	p.readFile("C:\\Code Workspace\\StockSimulator\\WorkspaceProject\\StockSimulatorBGSZ0.3\\bgszExchangePortfolio.csv");
+	Portfolio p = new Portfolio();{
+		
+		try {
+			p.readFile(GUIroughdraft.pathName);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		holdings = p.getHoldings();
+		System.out.println("Ticker= " + holdings[0] + "; NumShares= " 
+				+ holdings[1] + "; Price= " + holdings[2]);
+		
 	
-	
+	}
+
+
 }
 
